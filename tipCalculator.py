3e7eb533ed34f4bc5ffdf45f8bd43670dd93799d
bwire40 <emmanuelbwire80@gmail.com>
2024-04-08 09:43:31 +0300

    tipCalculator: math operators with integers and floats and strings using inputs

--- tipCalculator.py
+++ tipCalculator.py
@@ -6,8 +6,17 @@
 # math operators +, -, /, *, **, //, %
 
 
-food_amount = 100
-tip_percent = 20 / 100
+# food_amount = 100
+# tip_percent = 20 / 100
+# tip_amount = food_amount * tip_percent
+# total_amount = food_amount + tip_amount
+# print("$" + str(total_amount))
+#
+
+# use inputs make it dynamic
+food_amount = int(input("Enter food amount: "))
+tip_percent = int(input("Enter the tip percentage: "))/100
 tip_amount = food_amount * tip_percent
 total_amount = food_amount + tip_amount
 print("$" + str(total_amount))
+
