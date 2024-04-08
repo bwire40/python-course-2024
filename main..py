f29bef89bc270ddb8eee45974383e246b204f889
bwire40 <emmanuelbwire80@gmail.com>
2024-04-08 09:17:20 +0300

    Intro concepts: comments, variables, printing values

---
+++ main..py
@@ -0,0 +1,41 @@
+# comments - start with a harsh sign
+
+# variables
+# start with letters most of the time
+# print("my name")
+# name = "Bwire"
+# age = 25
+
+# first_name = "Emmanuel Bwire"
+# print(first_name)
+
+# multiple assignment
+# width, height = 400, 500
+# john = karo = patel = 50
+
+# print the output
+# print(width)
+# print(height)
+# print(john)
+# print(karo)
+# print(patel)
+
+# accepting inputs
+first_name = input("Enter Your first name: ")
+last_name = input("Enter the last name: ")
+
+# print("Hi " + first_name + " " + last_name)
+
+# simple calculator
+# num1=int(input("Num1: "))
+# num2=int(input("Num2: "))
+#
+# print(num2+num1)
+
+# data types
+# strings
+# int
+# float
+
+
+# string - anything in quotes
