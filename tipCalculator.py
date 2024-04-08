92a127b3f58d1a8c2f813b4543efe5a4fdacfeae
bwire40 <emmanuelbwire80@gmail.com>
2024-04-08 09:39:39 +0300

    tipCalculator: math operators with integers and floats

---
+++ tipCalculator.py
@@ -0,0 +1,13 @@
+# Tip calculator app
+# total=100
+# tip=20 per cent
+# total payment
+
+# math operators +, -, /, *, **, //, %
+
+
+food_amount = 100
+tip_percent = 20 / 100
+tip_amount = food_amount * tip_percent
+total_amount = food_amount + tip_amount
+print(total_amount)
