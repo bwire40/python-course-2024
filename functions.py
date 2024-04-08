0b273dc1f4d9199701e5d83f8ab2527fa26115aa
bwire40 <emmanuelbwire80@gmail.com>
2024-04-08 13:14:36 +0300

    More function practises

--- functions.py
+++ functions.py
@@ -1,54 +1,77 @@
-# functions - block of codes that can be reused when called
+# # functions - block of codes that can be reused when called
+#
+# # function definition def
+# def say_my_name():
+#     print('Bwire')
+#     print('Karol')
+#
+#
+# # arguments
+# def say_my_name2(name):
+#     print(name)
+#
+#
+# # say_my_name()  # function call
+# # say_my_name2("Bwire")
+#
+# def greeting(greet, name):
+#     print(f'{greet}! {name}')
+#
+#
+# # greeting('Aloha', 'bwire')
+# '''
+# multiple line comments for documentation using the triple quotation'''
+#
+#
+# # default arguments
+# # positional arguments
+# def greeting2(name, greet='Aloha'):
+#     print(f'{greet}, {name}')
+#
+#
+# # positional arguments
+# # greeting2("Bwire","Alohaa")
+#
+# # named arguments
+# # greeting2(name='Bwire',greet='Hello')
+#
+# # sum function, 2 numbers
+#
+# def sum_calc(a, b):
+#     """
+#     Take 2 integers, return their sum
+#     :param a:
+#     :param b:
+#     :return a+b:
+#
+#     This function can be imported to external files
+#
+#     """
+#     return a + b
+#
+#
+# # result = sum_calc(1, 2)  # returns 3 and stores in results
+# # print(result)
 
-# function definition def
-def say_my_name():
-    print('Bwire')
-    print('Karol')
-
-
-# arguments
-def say_my_name2(name):
-    print(name)
-
-
-# say_my_name()  # function call
-# say_my_name2("Bwire")
-
-def greeting(greet, name):
-    print(f'{greet}! {name}')
-
-
-# greeting('Aloha', 'bwire')
-'''
-multiple line comments for documentation using the triple quotation'''
-
-
-# default arguments
-# positional arguments
-def greeting2(name, greet='Aloha'):
-    print(f'{greet}, {name}')
-
-
-# positional arguments
-# greeting2("Bwire","Alohaa")
-
-# named arguments
-# greeting2(name='Bwire',greet='Hello')
-
-# sum function, 2 numbers
-
-def sum_calc(a, b):
+# weather app with functions
+def weather(condition):
     """
-    Take 2 integers, return their sum
-    :param a:
-    :param b:
-    :return a+b:
+    weather takes in 1 argument as a string
+    (expected values: 'couldy','thurnderstorm','sunny')
 
-    This function can be imported to external files
+    returns a response instruction for the user
 
+    :param condition:
+    :return:
     """
-    return a + b
+    if condition == 'rainy':
+        response = 'Umbrella'
+    elif condition == 'sunny':
+        response = 'Sun glasses'
+    else:
+        response = 'cloudy'
+
+    return response
 
 
-# result = sum_calc(1, 2)  # returns 3 and stores in results
-# print(result)
+print(weather(condition='rainy'))
--- tipCalculator.py
+++ tipCalculator.py
@@ -1,4 +1,7 @@
 # Tip calculator app
+
+# add functions
+
 # total=100
 # tip=20 per cent
 # total payment
@@ -11,18 +14,36 @@
 # tip_amount = food_amount * tip_percent
 # total_amount = food_amount + tip_amount
 # print("$" + str(total_amount))
+
+# def calculate_tip(f_amount, t_percent):
+#     """
 #
+#     :param f_amount:
+#     :param t_percent:
+#     :returns the tip_amount:
+#     """
+#     return f_amount * t_percent
+
 
 # use inputs make it dynamic
 food_amount = int(input("Enter food amount: "))
-tip_percent = int(input("Enter the tip percentage: ")) / 100
-print("********************************************")
-tip_amount = food_amount * tip_percent
-total_amount = food_amount + tip_amount
+tip_percent = int(input("Enter the tip percentage: "))
 
-# string formating
-print(f'Tip Amount: ${tip_amount}')
-print(f'Food Amount: ${food_amount}')
-print("***********************************")
-print('\n')  # new line
-print(f'Total Amount: ${total_amount}')
+print("********************************************")
+
+
+def calculate_food_total(f_amount, t_percentage):
+    """
+
+    :param f_amount:
+    :param t_percentage:
+    :returns the total amount by adding food and tip amount:
+    """
+    tip_amount = food_amount * (t_percentage / 100)
+
+    return f_amount + tip_amount
+
+
+total = calculate_food_total(food_amount, tip_percent)
+
+print(total)
