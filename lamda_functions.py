3b0c9b8e3df7349718cf5122d52e9ada9e29db2e
bwire40 <emmanuelbwire80@gmail.com>
2024-04-09 09:54:55 +0300

    lamda functions

---
+++ lamda_functions.py
@@ -0,0 +1,16 @@
+# lamda - anonymous function
+
+# normal function
+# def sum(a,b):
+#     return a+b
+
+# lamda function
+
+sum = lambda a, b: a + b
+
+# print(sum(2, 3))
+
+# impicit return
+
+# assert testing
+assert sum(1,2)==3, 'Error! Not return desired result'
