0098da455edbb7ed4705a87bcbf979b6e232a017
bwire40 <emmanuelbwire80@gmail.com>
2024-04-10 10:31:33 +0300

    while loops in python

--- forloops.py
+++ forloops.py
@@ -4,10 +4,24 @@ block of code for each element in the sequence"""
 fruits = ['mango', 'apples', 'banana', 'peas', 'peaches']
 
 # loop through the list and print each item in the list
-for fruit in fruits:
-    print(f'{fruit}')
-
+# for fruit in fruits:
+#     print(f'{fruit}')
 
 # # loop in range
 # for i in range(20):
 #     print(i + 1)   # prints 1-20
+
+my_fruit = input("Enter the fruit: ")
+# for i in [1, 2, 3, 4, 5]:
+#     fruits.append(my_fruit)  # adds my_fruit 5 times
+#     for fruit in fruits:
+#         print(fruit)
+#     # print(i)
+
+for i in [1, 2, 3, 4, 5]:
+    if my_fruit in fruits:
+        my_fruit = input("Enter the fruit: ")
+    else:
+        fruits.append(my_fruit)
+        for fruit in fruits:
+            print(fruit)
