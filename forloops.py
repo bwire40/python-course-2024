e44f02d4280c4d9fb374aa7a5d057625802aa9fb
bwire40 <emmanuelbwire80@gmail.com>
2024-04-10 10:20:26 +0300

    For loops in python

---
+++ forloops.py
@@ -0,0 +1,13 @@
+"""The for loop in Python is used to iterate over a sequence (such as a list, tuple, string, or range) and execute a
+block of code for each element in the sequence"""
+# for loops
+fruits = ['mango', 'apples', 'banana', 'peas', 'peaches']
+
+# loop through the list and print each item in the list
+for fruit in fruits:
+    print(f'{fruit}')
+
+
+# # loop in range
+# for i in range(20):
+#     print(i + 1)   # prints 1-20
