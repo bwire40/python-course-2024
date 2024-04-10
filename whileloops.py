ff97b859b5b9060568543a6667ea9e557c588cbb
bwire40 <emmanuelbwire80@gmail.com>
2024-04-10 11:02:33 +0300

    while loops in python

---
+++ whileloops.py
@@ -0,0 +1,6 @@
+"""A while loop in Python repeatedly executes a block of code as long as a specified condition evaluates to True. It
+continues iterating until the condition becomes False. The basic syntax of a while loop in Python is as follows:"""
+counter = 1
+while counter < 10:
+    print(counter)
+    counter = counter + 1
